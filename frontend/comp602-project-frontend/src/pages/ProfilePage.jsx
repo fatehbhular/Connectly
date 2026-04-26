000cda82278bfdca588d9438ad980c0c3d6a61b0
Srikar Kurani <srikarkurani@gmail.com>
2026-04-26 12:56:42 +1200

    Added state variables for profile fields

--- frontend/comp602-project-frontend/src/pages/ProfilePage.jsx
+++ frontend/comp602-project-frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 export default function ProfilePage() {
+
+  // State for profile form fields
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [skills, setSkills] = useState("");
+
   return (
     <div style={{ padding: "20px", paddingBottom: "80px" }}>
       <h1>Profile Page</h1>
