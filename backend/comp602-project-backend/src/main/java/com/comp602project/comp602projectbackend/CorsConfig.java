28d187c718a740a7e1ea1eaae2232db83c78f00e
Shawn Lee <yzshawnlee@gmail.com>
2026-05-21 18:45:04 +1200

    bug fix

--- backend/comp602-project-backend/src/main/java/com/comp602project/comp602projectbackend/CorsConfig.java
+++ backend/comp602-project-backend/src/main/java/com/comp602project/comp602projectbackend/CorsConfig.java
@@ -12,6 +12,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("https://connectly-production-3714.up.railway.app "); //change to proper backend later.
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://connectly-pink-six.vercel.app");
         config.addAllowedOriginPattern("*");    
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
